df93b6bb301cd45fe1fc5e3b0a5982625b211723
Owen Cruse <ocruse7@gmail.com>
2019-10-24 17:56:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,9 +16,9 @@ def fourth_challenge
       :heroine_friends => [
           {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
-      ]
-   }
-}
+        ]
+    }
+  }
 
   
 
